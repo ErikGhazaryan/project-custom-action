15f1f887789635709081d81c0a2b7bf59c81979e
Erik Ghazaryan <erik.ghazaryan.ext@riag-digital.com>
2025-02-04 12:13:28 +0100

    changed command in aws s3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -10,7 +10,7 @@ function run() {
 
     // 2) Upload my files
     const s3Uri = `s3://${bucket}`;
-    exec.exec(`aws s3 sync  ${distFolder} ${s3Uri}-- region ${bucketRegion}`);
+    exec.exec(`aws s3 sync  ${distFolder} ${s3Uri} --region ${bucketRegion}`);
     
     // Get some input values
     

e8f10f78b0c20a82c8f7d413c61fcc9e2e571c5a
Erik Ghazaryan <erik.ghazaryan.ext@riag-digital.com>
2025-02-04 11:41:42 +0100

    changed in require

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -1,6 +1,6 @@
-const core = requre('@actions/core');
-const github = requre('@actions/github');
-const exec = requre('@actions/exec');
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
 
 function run() {
     core.notice('Hello from my custom Javascript Action!');
